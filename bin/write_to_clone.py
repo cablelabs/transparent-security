57e411f66ce34e493799c87360dd7ebd4ab5cff6
Sanjhana-Jayagopal <s.jayagopal@cablelabs.com>
2020-02-28 15:10:19 -0700

    Core tests for UDP IPv4/IPv6

---
+++ bin/write_to_clone.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python2
+
+# Copyright (c) 2019 Cable Television Laboratories, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+import logging
+import sys
+
+import yaml
+
+from trans_sec.p4runtime_lib.bmv2 import Bmv2SwitchConnection
+from trans_sec.p4runtime_lib.helper import P4InfoHelper
+
+logger = logging.getLogger('insert_p4_clone_session_entry')
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--grpc_addr', type=str, required=True,
+                        help='Switch grpc host:port')
+    parser.add_argument('-d', '--dev_id', type=int, required=False, default=0,
+                        help='The device ID (default 0)')
+    parser.add_argument(
+        '-p', '--p4-info-fpath', type=str, required=False, default=0,
+        help='The file path of the switch associated p4info file')
+    parser.add_argument('-l', '--log-dir', type=str, required=False,
+                        default=None)
+    parser.add_argument('-lf', '--log-file', type=str, required=False,
+                        default='insert_p4_table.log')
+    parser.add_argument('-c', '--clone', type=str, required=False,
+                        default='True', help='Build clone entry')
+    parser.add_argument('-ce', '--clone-egress', type=int, required=False,
+                        default=0, help='Clone egress port')
+    return parser.parse_args()
+
+
+def read_yaml_file(config_file_path):
+    """
+    Reads a yaml file and returns a dict representation of it
+    :return: a dict of the yaml file
+    """
+    logger.debug('Attempting to load configuration file - ' + config_file_path)
+    config_file = None
+    try:
+        with open(config_file_path, 'r') as config_file:
+            config = yaml.safe_load(config_file)
+            logger.info('Loaded configuration')
+        return config
+    finally:
+        if config_file:
+            logger.info('Closing configuration file')
+            config_file.close()
+
+
+if __name__ == '__main__':
+    """
+    Inserts clone entries based on the table config file
+    """
+    args = get_args()
+    if args.log_dir and args.log_file:
+        log_file = '{}/{}'.format(args.log_dir, args.log_file)
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG,
+                            filename=log_file)
+    else:
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+    logger.info('Connecting to BMV2 Switch #[%s] at [%s] loaded with P4 [%s]',
+                args.dev_id, args.grpc_addr, args.p4_info_fpath)
+    p4info_helper = P4InfoHelper(args.p4_info_fpath)
+    switch = Bmv2SwitchConnection(
+        name='test', address=args.grpc_addr, device_id=args.dev_id)
+    switch.master_arbitration_update()
+
+    if args.clone == 'True':
+        clone_entry = p4info_helper.build_clone_entry(args.clone_egress)
+        logger.info('Inserting clone entry [%s]', clone_entry)
+        switch.write_clone_entries(clone_entry)
+    else:
+        clone_entry = p4info_helper.build_clone_entry(args.clone_egress)
+        logger.info('Deleting clone entry [%s]', clone_entry)
+        switch.delete_clone_entries(clone_entry)
