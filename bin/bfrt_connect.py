98a8900881ea90b674a1424e5709037c1209f27f
spisarski <s.pisarski@cablelabs.com>
2020-08-31 16:23:55 -0600

    Fix as per Barefoot as the P4 program name cannot contain a dash character '-' Able to retrieve BF Info and beginning to attempt to perform first table insert

--- bin/bfrt_connect.py
+++ bin/bfrt_connect.py
@@ -42,12 +42,22 @@ def get_args():
                         default=False, help='Is master client')
     parser.add_argument('-t', '--table-name', required=False,
                         help='The table name for logging the ID')
+    parser.add_argument('-ct', '--clear-tables', required=False, type=bool,
+                        default=False,
+                        help='The table name for logging the ID')
 
     parser.add_argument('-l', '--log-dir', type=str, required=False,
                         default=None)
     parser.add_argument('-lf', '--log-file', type=str, required=False,
                         default='insert_p4_table.log')
     return parser.parse_args()
+
+
+def get_table_id(tbl_info, in_table_name):
+    for table_name, table in tbl_info.table_dict.items():
+        if table_name == in_table_name:
+            logger.info('Table [%s] found with ID [%s]', table_name, table.info.id)
+            return table.info.id
 
 
 if __name__ == '__main__':
@@ -82,37 +92,48 @@ if __name__ == '__main__':
     interface = bfrt_client.ClientInterface(
         grpc_addr=args.grpc_addr, client_id=args.client_id,
         device_id=args.device_id, is_master=args.is_master)
-    logger.info('Clearing tables')
-    interface.clear_all_tables()
-    logger.info('Completed clearing tables')
-
-    # Attempt to receive the ID for the args.table_name argument
-    if args.table_name:
-        logger.info('Retrieve table name - [%s]', args.table_name)
-        req = bfruntime_pb2.ReadRequest()
-        req.client_id = args.client_id
-        req.target.device_id = args.device_id
-
-        object_id = req.entities.add().object_id
-        object_id.table_object.table_name = args.table_name
-
-        ex = None
-        try:
-            for rep in client_stub.Read(req):
-                logger.info('Table ID - [%s]', rep.entities[0].object_id.id)
-        except Exception as e:
-            ex = e
-        finally:
-            if ex:
-                logger.error("Exit with error - [%s]", ex)
-                raise ex
-            else:
-                logger.info('Graceful exit')
-                exit(0)
 
     # interface.bind_pipeline_config(args.switch_name)
     # bfrt_info = interface.bind_pipeline_config(args.switch_name)
+    logger.info('Retrieving bfrt_info for switch - [%s]', args.switch_name)
     bfrt_info = interface.bfrt_info_get(args.switch_name)
+    logger.info('Table info - [%s]', bfrt_info)
+    logger.info('Table info class - [%s]', bfrt_info.__class__)
+
+    if args.clear_tables:
+        logger.info('Clearing tables')
+        interface.clear_all_tables()
+        logger.info('Completed clearing tables')
+
+    # Attempt to receive the ID for the args.table_name argument
+    if args.table_name:
+        table_info = get_table_id(bfrt_info, args.table_name)
+        logger.info('table_info = %s', table_info)
+
+        if not table_info:
+            logger.info('Retrieve table name - [%s]', args.table_name)
+            req = bfruntime_pb2.ReadRequest()
+            req.client_id = args.client_id
+            req.target.device_id = args.device_id
+
+            # entity = req.entities.add()
+            # logger.debug('entity = ', entity)
+            object_id = req.entities.add().object_id
+            object_id.table_object.table_name = args.table_name
+
+            ex = None
+            try:
+                for rep in client_stub.Read(req):
+                    logger.info('Table ID - [%s]', rep.entities[0].object_id.id)
+            except Exception as e:
+                ex = e
+            finally:
+                if ex:
+                    logger.error("Exit with error - [%s]", ex)
+                    raise ex
+                else:
+                    logger.info('Graceful exit')
+                    exit(0)
 
     logger.info('Exit 0')
     exit()
