ce6f39dc5c4506231dc0d12a07b6dc54e963d341
spisarski <s.pisarski@cablelabs.com>
2020-11-02 11:57:24 -0700

    changed core_tna.p4 data_inspection_t table name to add_switch_id_t like in aggregate_tna.p4 Updated snaps-hcp AMI to fast install version Simplified single_switch data-inspection tests Created web service for adding/removing entries from setup_telem_rpt_t table in core_tna.p4

--- automation/p4/tofino/variables.tf
+++ automation/p4/tofino/variables.tf
@@ -35,7 +35,7 @@ variable "tofino" {
     bfrt_ami = "ami-01a5ff54de23b6739"
   }
 }
-variable "hcp_ami" {default ="ami-06798bd7f2900cf9e"}
+variable "hcp_ami" {default ="ami-07c9ba265e178c9a3"}
 
 variable "switch_instance_type" {default = "t2.2xlarge"}
 variable "ae_instance_type" {default = "t2.large"}
