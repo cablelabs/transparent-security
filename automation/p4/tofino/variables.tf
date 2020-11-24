9e4ece4daf7793e393632073384e335e2d5cff0b
Ashwini Kadam <a.kadam@cablelabs.com>
2020-11-24 12:17:01 -0800

    Updated AMI ID

--- automation/p4/tofino/variables.tf
+++ automation/p4/tofino/variables.tf
@@ -35,7 +35,7 @@ variable "tofino" {
     bfrt_ami = "ami-01a5ff54de23b6739"
   }
 }
-variable "hcp_ami" {default ="ami-0bc9ab59c9cd89a82"}
+variable "hcp_ami" {default ="ami-03f2646bda911dbc2"}
 
 variable "switch_instance_type" {default = "t2.2xlarge"}
 variable "ae_instance_type" {default = "t2.large"}
